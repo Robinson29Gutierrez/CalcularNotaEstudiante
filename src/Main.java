9fd3be7c16e6371bd0cfe965f63bcc017db4c7f1
CarolinaBolivar5 <cbolivar12@cesde.net>
2025-04-09 20:23:56 -0500

    estructura de calculo de notas

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        // desarrollar un sistema de notas para un estudiante que desea saber su nota final 8de cada materia)
+        // el sistema debe pedir el nombre del estudiante , el documento  y el programa  al que esta matriculado
+        //(menu de opciones : Desarollo de software, Diseño grafico, Gastronomia)
+        // de permitir selecionar la materia (menu opciones: desarrollo: logica, metodologias, Introduccion ,diseño:
+        // tipografia ,iconografia , semantica.
+        // gastronomia: costos , panaderia dulce, cocina internacional)
+        // El calculo de notas es de la siguiente manera :
+        // para cada materia hay un examen paracial que equivale  al 20% de la nota , un taller que equivale
+        // el 30% de la nota y un examen fianl que equivale el 50% de la nota)
+        //una vez calculada la nota final tener en cuenta que:
+        // si la nota esta entre 0 y 2 el estudiante pierde definitivamente
+        // si la nota esta entre 2.1 y 2.9 tiene derecho a plan de mojoramiento.
+
+
+
+
+
+
+
+
+
+    }
+}
